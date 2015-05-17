b46a233d36ae33de46824d0604b7befd17dd6525
iAntonioPerez <antonioperez@mail.fresnostate.edu>
2015-05-17 16:17:09 -0700

    files

---
+++ Web/forms/forms.go
@@ -0,0 +1,31 @@
+package forms
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type user struct {
+	Name  string
+	Email string
+}
+
+var templates *template.Template
+
+func init() {
+	http.HandleFunc("/", login)
+	http.HandleFunc("/info", info)
+	templates = template.Must(template.ParseFiles("login.html", "info.html"))
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "login.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func info(w http.ResponseWriter, r *http.Request) {
+	currentuser := user{r.FormValue("name"), r.FormValue("email")}
+	templates.ExecuteTemplate(w, "info.html", currentuser)
+}
